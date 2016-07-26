80e4f55c2b681b0fa35ebff6952d9adb23aed141
eldh <andreas.eldh@gmail.com>
2016-07-26 23:57:40 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const test = {b: 'a'}
+console.log({test, c: 'c'})
+
+// console.log(require("react"))
---
+++ src/index.js
@@ -0,0 +1,2 @@
+console.log('Hello, welcome to Jour src')
+console.log(require("react"))
