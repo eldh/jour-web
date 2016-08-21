a10a595ed4597e345b1b690adb3fa17f7e37a81d
eldh <andreas.eldh@gmail.com>
2016-08-21 18:12:05 +0200

    fb disclaimer

--- src/components/LoggedOutUserInfo.js
+++ src/components/LoggedOutUserInfo.js
@@ -21,14 +21,27 @@ const textStyles = {
   marginBottom: space,
 }
 
+const disclaimerStyles = {
+  fontSize: 12,
+  lineHeight: 1.5,
+  textAlign: 'center',
+  color: '#64656e',
+  marginTop: space,
+}
+
 const welcomeText = `Welcome to Jour.
-A minimal online diary.
-`
+A minimal online diary.`
+
+const disclaimer = `Facebook is only used for authentication.
+We won't share anything with anyone.
+Your diary is completely private.`
+
 class UserInfo extends Component {
   render() {
     return h(Box, {styles: containerStyles},
       h(Pre, {styles: textStyles}, welcomeText),
-      h(SignInButton, this.props)
+      h(SignInButton, this.props),
+      h(Pre, {styles: disclaimerStyles}, disclaimer)
     )
   }
 }
